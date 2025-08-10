10e14fbe8353af4c293b43648f62b6cbb24c5081
kanshurichard <zk0523@hotmail.com>
2025-08-10 10:13:51 +0800

    Update enable_ai.sh

--- enable_ai.sh
+++ enable_ai.sh
@@ -4,8 +4,8 @@ set -e
 
 # --- Initial Welcome ---
 echo "==================================================================="
-echo " macOS Apple Intelligence Enablement Script 2.12 by KanShuRichard"
-echo "       macOS Apple 智能启用辅助脚本 2.12 by KanShuRichard"
+echo " macOS Apple Intelligence Enablement Script 2.13 by KanShuRichard"
+echo "       macOS Apple 智能启用辅助脚本 2.13 by KanShuRichard"
 echo "==================================================================="
 
 # --- Language Selection ---
